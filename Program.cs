28a6f85db28fc00d9d83745055349a3f7c5104a0
Naza990 <146908951+Naza990@users.noreply.github.com>
2023-10-13 03:32:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2 ,c3 ,c4 ,c5 ,c6 ;
+            float promedio;
+            Console.WriteLine("Escribe tu primera promedio ");
+            c1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu primera promedio ");
+            c2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu primera promedio ");
+            c3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu primera promedio ");
+            c4= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu primera promedio ");
+            c5 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu primera promedio ");
+            c6 = Convert.ToInt32(Console.ReadLine());
+            promedio= c1 + c2+c3+c4+c5+c6/6:
+                Console.WriteLine("Tu promedio es :" + promedio);
+        }
+    }
+}
