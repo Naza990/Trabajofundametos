25888e8e389dad6022fc247e5875085bfde2799c
Naza990 <146908951+Naza990@users.noreply.github.com>
2023-10-13 04:22:25 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,27 +6,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            int c1, c2 ,c3 ,c4 ,c5 ,c6 ;
-            float promedio;
-            Console.WriteLine("Escribe tu primera promedio ");
-            c1= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa la longitud en metros: ");
 
-            Console.WriteLine("Escribe tu primera promedio ");
-            c2 = Convert.ToInt32(Console.ReadLine());
+            // Lee la entrada del usuario y la convierte a un valor decimal
+            if (decimal.TryParse(Console.ReadLine(), out decimal metros))
+            {
+                // Realiza la conversión de metros a pies
+                decimal pies = metros * 3.28084m;
 
-            Console.WriteLine("Escribe tu primera promedio ");
-            c3 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Escribe tu primera promedio ");
-            c4= Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Escribe tu primera promedio ");
-            c5 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Escribe tu primera promedio ");
-            c6 = Convert.ToInt32(Console.ReadLine());
-            promedio= c1 + c2+c3+c4+c5+c6/6:
-                Console.WriteLine("Tu promedio es :" + promedio);
+                // Muestra el resultado
+                Console.WriteLine($"{metros} metros equivalen a {pies} pies.");
+            }
         }
     }
 }
