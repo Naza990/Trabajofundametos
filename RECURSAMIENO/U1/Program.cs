4e6a7099f53f393ced585688d94337c311ec547e
Naza990 <146908951+Naza990@users.noreply.github.com>
2023-12-11 00:09:07 -0500

    Add files via upload

---
+++ RECURSAMIENO/U1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Ingrese el número de días: ");
+        int totalDias = Convert.ToInt32(Console.ReadLine());
+
+        // Calcular años, meses, semanas y días
+        int A = totalDias / 365;
+        int B = totalDias % 365;
+        int C = B / 30;
+        B = B % 30;
+        int semanas = B / 7;
+        int dias = B % 7;
+
+        // Mostrar el resultado
+        Console.WriteLine($"Corresponden a: {A} años, {C} meses, {semanas} semanas y {dias} días.");
+    }
+}
