2d24c54940932257312a4eef2d60dc599a031bf3
Naza990 <146908951+Naza990@users.noreply.github.com>
2023-12-11 02:09:28 -0500

    Add files via upload

--- Unidad 5 /Java/c#/Program.cs
+++ Unidad 5 /Java/c#/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace U5_P5
+{
+    internal class U5_P5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una cadena o palabras de texto: ");
+            string texto = Console.ReadLine();
+
+            int cantidadPalabras = ContarPalabras(texto);
+
+            Console.WriteLine("Número de palabras: " + cantidadPalabras);
+        }
+
+        public static int ContarPalabras(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+            {
+                return 0;
+            }
+
+            string[] palabras = texto.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return palabras.Length;
+        }
+    }
+
+}
