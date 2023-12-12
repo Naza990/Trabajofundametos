dd448d52c172a8e1fa4a3e07c280a913d76b3292
Naza990 <146908951+Naza990@users.noreply.github.com>
2023-12-11 18:23:00 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-
-namespace Codigo8
+namespace Prueba
 {
-    internal class Program
+    internal class Prueba
     {
         static void Main(string[] args)
         {
-
-            int Area, a, p;
-            
-            Console.Write("Introduce el valor de tu Apotema:");
-            a = Console.ReadLine();
-
-            Console.Write("introduce el valor de tu Perimetro:");
-            p = Convert.ToInt32(Console.ReadLine());
-
-            Area = (a * p) / 2;
-            Console.Write($" El Area de su Hexagono {Area}");
-
+            double S = 1500, I = 0.10, i, s = 0;
+            int años = 6;
+            for (i = 1; i <= años && años > 0; i++)
+            {
+                S = (S * I) + S;
+                Console.WriteLine("El salario anual " + i + " es de: $" + S);
+                s = s + S;
+            }
+            Console.WriteLine("El salario a los 6 años es: $" + s);
         }
     }
 }
